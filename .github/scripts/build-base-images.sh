153fa9d6c20a26f70b9649d42ef0101cacbaf2aa
Trevor Bossert <alanboss@gmail.com>
2023-05-12 11:48:53 -0700

    Build multi-platform images

--- .github/scripts/build-base-images.sh
+++ .github/scripts/build-base-images.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-quay.io/unstructured-io}"
 PIP_VERSION="${PIP_VERSION:-22.2.1}"
 GITHUB_REF="${GITHUB_REF:-none}"
+DOCKER_PLATFORM="${DOCKER_PLATFORM:-linux/amd64,linux/arm64}"
 
 cd dockerfiles
+
+docker buildx create --use
 for DOCKERFILE in *; do
     DOCKERFILE_NAME=$(basename "$DOCKERFILE")
     DOCKER_IMAGE="${DOCKER_IMAGE:-$DOCKER_REPOSITORY/$DOCKERFILE_NAME}"
 
-    DOCKER_BUILD_CMD=(docker buildx build --load -f Dockerfile \
+    DOCKER_BUILD_CMD=(docker buildx build --push \
     --build-arg PIP_VERSION="$PIP_VERSION" \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     --progress plain \
