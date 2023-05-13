470b081186fd63f5758c6ca8d58b148c0374c88b
Trevor Bossert <alanboss@gmail.com>
2023-05-12 19:13:50 -0700

    maybe I don’t want push …

--- .github/scripts/build-base-images.sh
+++ .github/scripts/build-base-images.sh
@@ -13,7 +13,7 @@ for DOCKERFILE in *; do
     DOCKERFILE_NAME=$(basename "$DOCKERFILE")
     DOCKER_IMAGE="${DOCKER_IMAGE:-$DOCKER_REPOSITORY/$DOCKERFILE_NAME}"
 
-    DOCKER_BUILD_CMD=(docker buildx build --push \
+    DOCKER_BUILD_CMD=(docker buildx build \
     --build-arg PIP_VERSION="$PIP_VERSION" \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     --progress plain \
