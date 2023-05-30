8484f9666f2672ccdd135cddaf4bb61951bb68fb
Trevor Bossert <alanboss@gmail.com>
2023-05-30 13:24:14 -0700

    Get rid of centos dockerfile
    Minor changes to build script

--- .github/scripts/build-base-images.sh
+++ .github/scripts/build-base-images.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-quay.io/unstructured-io}"
 PIP_VERSION="${PIP_VERSION:-22.2.1}"
 GITHUB_REF="${GITHUB_REF:-none}"
-DOCKER_PLATFORM="${DOCKER_PLATFORM:-linux/amd64}"
-cd dockerfiles
+DOCKER_PLATFORM="${DOCKER_PLATFORM:-}"
 
+cd dockerfiles
 docker buildx create --use
 for DOCKERFILE in *; do
     DOCKERFILE_NAME=$(basename "$DOCKERFILE")
     DOCKER_IMAGE="${DOCKER_IMAGE:-$DOCKER_REPOSITORY/$DOCKERFILE_NAME}"
 
-    BUILDX_COMMAND="docker buildx build"
+    BUILDX_COMMAND="docker buildx build --load"
     if [ "$GITHUB_REF" == "refs/heads/main" ]; then
         BUILDX_COMMAND="docker buildx build --push"
     fi
@@ -31,8 +31,4 @@ for DOCKERFILE in *; do
     fi
 
     DOCKER_BUILDKIT=1 "${DOCKER_BUILD_CMD[@]}"
-
-    if [ "$GITHUB_REF" == "refs/heads/main" ]; then
-        docker push "${DOCKER_IMAGE}"
-    fi
 done
