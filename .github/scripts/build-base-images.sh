4f791ceff96d4cc5668b497e975654b94c8c4f29
Trevor Bossert <alanboss@gmail.com>
2023-06-30 16:40:03 -0700

    push build changes

--- .github/scripts/build-base-images.sh
+++ .github/scripts/build-base-images.sh
@@ -4,37 +4,43 @@ set -euo pipefail
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-quay.io/unstructured-io}"
 PIP_VERSION="${PIP_VERSION:-22.2.1}"
 GITHUB_REF="${GITHUB_REF:-none}"
-DOCKER_PLATFORM="${DOCKER_PLATFORM:-}"
+DOCKER_PLATFORM="${DOCKER_PLATFORM:-linux/amd64}"
+DOCKERFILE="${DOCKERFILE:-}"
 CI="${CI:-false}"
+
+if [ -z "$DOCKERFILE" ]; then
+    echo "DOCKERFILE is not set"
+    exit 1
+fi
+
+ARCH=$(echo $DOCKER_PLATFORM |sed 's/\// /g' |awk '{print $2}')
 
 cd dockerfiles
 docker buildx create --use
-for DOCKERFILE in *; do
-    DOCKERFILE_NAME=$(basename "$DOCKERFILE")
-    DOCKER_IMAGE="${DOCKER_IMAGE:-$DOCKER_REPOSITORY/$DOCKERFILE_NAME}"
+DOCKERFILE=$(basename "$DOCKERFILE")
+DOCKER_IMAGE="${DOCKER_IMAGE:-$DOCKER_REPOSITORY/$DOCKERFILE}-$ARCH"
 
-    BUILDX_COMMAND=(docker buildx build)
-    if [ "$GITHUB_REF" == "refs/heads/main" ]; then
-        BUILDX_COMMAND+=("--push")
-    fi
+BUILDX_COMMAND=(docker buildx build)
+if [ "$GITHUB_REF" == "refs/heads/main" ]; then
+    BUILDX_COMMAND+=("--push")
+fi
 
-    if [ "$CI" == "false" ]; then
-        # (tabossert) This is a local build, so we want to load the image into the local docker daemon
-        # Buildx does not support loading multi-arch builds, so CI needs to skip loading.
-        BUILDX_COMMAND+=("--load")
-    fi
+if [ "$CI" == "false" ]; then
+    # (tabossert) This is a local build, so we want to load the image into the local docker daemon
+    # Buildx does not support loading multi-arch builds, so CI needs to skip loading.
+    BUILDX_COMMAND+=("--load")
+fi
 
-    # shellcheck disable=SC2206
-    DOCKER_BUILD_CMD=("${BUILDX_COMMAND[@]}" \
-    --build-arg PIP_VERSION="$PIP_VERSION" \
-    --build-arg BUILDKIT_INLINE_CACHE=1 \
-    --progress plain \
-    -t "$DOCKER_IMAGE" -f "$DOCKERFILE_NAME" .)
+# shellcheck disable=SC2206
+DOCKER_BUILD_CMD=("${BUILDX_COMMAND[@]}" \
+--build-arg PIP_VERSION="$PIP_VERSION" \
+--build-arg BUILDKIT_INLINE_CACHE=1 \
+--progress plain \
+-t "$DOCKER_IMAGE" -f "$DOCKERFILE" .)
 
-    # only build for specific platform if DOCKER_PLATFORM is set
-    if [ -n "${DOCKER_PLATFORM:-}" ]; then
-        DOCKER_BUILD_CMD+=("--platform=$DOCKER_PLATFORM")
-    fi
+# only build for specific platform if DOCKER_PLATFORM is set
+if [ -n "${DOCKER_PLATFORM:-}" ]; then
+    DOCKER_BUILD_CMD+=("--platform=$DOCKER_PLATFORM")
+fi
 
-    DOCKER_BUILDKIT=1 "${DOCKER_BUILD_CMD[@]}"
-done
+DOCKER_BUILDKIT=1 "${DOCKER_BUILD_CMD[@]}"
--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 PROJECT_DIR ?= $(realpath $(dir $(firstword $(MAKEFILE_LIST))))
 DOCKER_PLATFORM:= $(shell echo $(DOCKER_PLATFORM))
+DOCKERFILE:= $(shell echo $(DOCKERFILE))
 CI:= $(shell echo $(CI))
 
 .PHONY: help
@@ -8,4 +9,4 @@ help: Makefile
 
 .PHONY: build-base-images
 build-base-images:
-	DOCKER_PLATFORM=$(DOCKER_PLATFORM) CI=$(CI) $(PROJECT_DIR)/.github/scripts/build-base-images.sh
+	DOCKER_PLATFORM=$(DOCKER_PLATFORM) DOCKERFILE=$(DOCKERFILE) CI=$(CI) $(PROJECT_DIR)/.github/scripts/build-base-images.sh
