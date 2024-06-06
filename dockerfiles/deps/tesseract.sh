b99249ec56dfb8bb5b0354042ac8c47c58c55219
Michal Martyniak <michal.martyniak@deepsense.ai>
2024-06-06 12:34:11 +0200

    fix typo in pkgconfig (was pkconfig)

--- dockerfiles/deps/tesseract.sh
+++ dockerfiles/deps/tesseract.sh
@@ -23,7 +23,7 @@ cd ..
 git clone https://github.com/tesseract-ocr/tessdata.git
 cp tessdata/*.traineddata /usr/local/share/tessdata
 rm -rf /tesseract-ocr /tessdata /autoconf-archive-2017.09.28* /leptonica-1.83.1*
-dnf -y remove opencv* perl-core clang libpng-devel libtiff-devel libwebp-devel libjpeg-devel libjpeg-turbo-devel git-core libtool zlib-devel pkconfig xz
+dnf -y remove opencv* perl-core clang libpng-devel libtiff-devel libwebp-devel libjpeg-devel libjpeg-turbo-devel git-core libtool zlib-devel pkgconfig xz
 
 # General cleanup
 rm -rf /var/cache/yum/*
