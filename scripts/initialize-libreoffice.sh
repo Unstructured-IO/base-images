bb18b1c867b17fd31b9c65ce4b9f464f54226520
Matt Robinson <mrobinson@unstructuredai.io>
2024-06-19 18:38:30 -0400

    tidy shell

--- scripts/initialize-libreoffice.sh
+++ scripts/initialize-libreoffice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$ARCH" != "arm64" ] && [ "$ARCH" != "aarch64" ]; then
-    /usr/bin/soffice --headless || [ $? -eq 81 ] || exit 1
+  /usr/bin/soffice --headless || [ $? -eq 81 ] || exit 1
 fi
