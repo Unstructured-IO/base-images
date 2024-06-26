822ba6bde6013998708c732011ef9cde0b02cd3d
Matt Robinson <mrobinson@unstructuredai.io>
2024-06-26 08:41:04 -0400

    switch to installing libreoffice from package manager

--- scripts/install-wolfi-amd64-packages.sh
+++ scripts/install-wolfi-amd64-packages.sh
@@ -3,12 +3,12 @@
 ARCH=$(uname -m)
 
 if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]]; then
-  apk add --allow-untrusted packages/libreoffice-7.6.5-r0.apk &&
-    mv /share/tessdata/configs /usr/local/share/tessdata/ &&
-    mv /share/tessdata/tessconfigs /usr/local/share/tessdata/ &&
-    ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice &&
-    ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice &&
-    chmod +x /usr/local/lib/libreoffice/program/soffice.bin &&
-    chmod +x /usr/bin/libreoffice &&
+  apk add libreoffice && \
+    mv /share/tessdata/configs /usr/local/share/tessdata/ && \
+    mv /share/tessdata/tessconfigs /usr/local/share/tessdata/ && \
+    /usr/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice && \
+    ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/soffice && \
+    chmod +x /usr/lib/libreoffice/program/soffice.bin && \
+    chmod +x /usr/bin/libreoffice && \
     chmod +x /usr/bin/soffice
 fi
