bd37d039a459f4df5a7c143716f8ca4f8bb95dc5
Matt Robinson <mthw.wm.robinson@gmail.com>
2024-06-26 16:33:28 -0400

    Revert "OSS-38 build: add libreoffice to wolfi arm64 (#32)"
    This reverts commit 0afafce28ea196304e2e0ce375db6d072264d4b6.

--- scripts/initialize-libreoffice.sh
+++ scripts/initialize-libreoffice.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/bin/soffice --headless || [ $? -eq 81 ] || exit 1
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]]; then
+  /usr/bin/soffice --headless || [ $? -eq 81 ] || exit 1
+fi
--- scripts/install-wolfi-libreoffice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apk add libreoffice
-ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
-ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/soffice
-chmod +x /usr/lib/libreoffice/program/soffice.bin
-chmod +x /usr/bin/libreoffice
-chmod +x /usr/bin/soffice
