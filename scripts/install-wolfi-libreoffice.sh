d46498ee35b3f4035e779896b9b4c114261b566e
Matt Robinson <mrobinson@unstructured.io>
2024-06-28 13:45:02 -0400

    fix: revert back to `libreoffice7` in `amd64` build (#35)
    * fix: correct short sha env var syntax in publish
    
    * fix: revert to libreoffice7 for amd64
    
    * linting, linting, linting
    
    * remove python3.12; adjust scan threshold

--- scripts/install-wolfi-libreoffice.sh
+++ scripts/install-wolfi-libreoffice.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-apk add libreoffice
-ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
-ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/soffice
-chmod +x /usr/lib/libreoffice/program/soffice.bin
-chmod +x /usr/bin/libreoffice
-chmod +x /usr/bin/soffice
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]]; then
+  apk add --allow-untrusted packages/libreoffice-7.6.5-r0.apk
+  ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
+  ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice
+  chmod +x /usr/local/lib/libreoffice/program/soffice.bin
+  chmod +x /usr/bin/libreoffice
+  chmod +x /usr/bin/soffice
+else
+  apk add libreoffice
+  ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
+  ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/soffice
+  chmod +x /usr/lib/libreoffice/program/soffice.bin
+  chmod +x /usr/bin/libreoffice
+  chmod +x /usr/bin/soffice
+fi
