6acecea7d093ee60bb8fce6f52efd70d717b4348
Matt Robinson <mrobinson@unstructuredai.io>
2024-07-02 10:16:36 -0400

    update to use new libreoffice sdks

--- scripts/install-wolfi-libreoffice.sh
+++ scripts/install-wolfi-libreoffice.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-ARCH=$(uname -m)
-
-if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "amd64" ]]; then
-  apk add --allow-untrusted packages/libreoffice-7.6.5-r0.apk
-  ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
-  ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice
-  chmod +x /usr/local/lib/libreoffice/program/soffice.bin
-  chmod +x /usr/bin/libreoffice
-  chmod +x /usr/bin/soffice
-else
-  apk add libreoffice
-  ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
-  ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/soffice
-  chmod +x /usr/lib/libreoffice/program/soffice.bin
-  chmod +x /usr/bin/libreoffice
-  chmod +x /usr/bin/soffice
-fi
+apk add --allow-untrusted packages/libreoffice-7.6.5-r0.apk
+ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
+ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice
+chmod +x /usr/local/lib/libreoffice/program/soffice.bin
+chmod +x /usr/bin/libreoffice
+chmod +x /usr/bin/soffice
