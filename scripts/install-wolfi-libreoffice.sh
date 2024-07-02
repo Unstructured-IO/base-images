67c8b0553a1fa71799cade123cdc17a796ee1eee
Matt Robinson <mrobinson@unstructuredai.io>
2024-07-02 10:30:07 -0400

    update libreoffice apk name

--- scripts/install-wolfi-libreoffice.sh
+++ scripts/install-wolfi-libreoffice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apk add --allow-untrusted packages/libreoffice-7.6.5-r0.apk
+apk add --allow-untrusted packages/libreoffice-7.6.7.2-r0.apk
 ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
 ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice
 chmod +x /usr/local/lib/libreoffice/program/soffice.bin
