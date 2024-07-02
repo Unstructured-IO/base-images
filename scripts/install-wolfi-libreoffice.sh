6d703b403a82af111e3a11ecbb4a7f1ef0790072
Matt Robinson <mrobinson@unstructuredai.io>
2024-07-02 10:35:40 -0400

    try other path

--- scripts/install-wolfi-libreoffice.sh
+++ scripts/install-wolfi-libreoffice.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apk add --allow-untrusted packages/libreoffice-7.6.7.2-r0.apk
-ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
-ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice
-chmod +x /usr/local/lib/libreoffice/program/soffice.bin
+ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice
+ln -s /usr/lib/libreoffice/program/soffice.bin /usr/bin/soffice
+chmod +x /usr/lib/libreoffice/program/soffice.bin
 chmod +x /usr/bin/libreoffice
 chmod +x /usr/bin/soffice
