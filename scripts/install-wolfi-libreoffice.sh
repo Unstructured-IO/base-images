1c7134b11f9e10c7867bad0200806c63301bc246
Matt Robinson <mrobinson@unstructuredai.io>
2024-08-09 14:49:04 -0400

    tidy shell

--- scripts/install-wolfi-libreoffice.sh
+++ scripts/install-wolfi-libreoffice.sh
@@ -2,8 +2,8 @@
 
 apk add --allow-untrusted packages/libreoffice-24-24.2.5.2-r1.apk
 
-ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice && \
-ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice && \
-chmod +x /usr/local/lib/libreoffice/program/soffice.bin && \
-chmod +x /usr/bin/libreoffice
+ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/libreoffice &&
+  ln -s /usr/local/lib/libreoffice/program/soffice.bin /usr/bin/soffice &&
+  chmod +x /usr/local/lib/libreoffice/program/soffice.bin &&
+  chmod +x /usr/bin/libreoffice
 chmod +x /usr/bin/soffice
