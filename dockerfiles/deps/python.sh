828dc12f767d11c436faa6766c3bb037d3b7e1c9
Yuming Long <yuming@unstructured.io>
2024-10-17 17:00:39 -0700

    upgrade python3.12

--- dockerfiles/deps/python.sh
+++ dockerfiles/deps/python.sh
@@ -6,14 +6,14 @@ dnf -y install bzip2-devel libffi-devel make git sqlite-devel openssl-devel
 dnf -y install python-pip
 pip3.9 install --upgrade setuptools pip
 dnf -y groupinstall "Development Tools"
-curl -O https://www.python.org/ftp/python/3.10.14/Python-3.10.14.tgz
-tar -xzf Python-3.10.14.tgz
-cd Python-3.10.14/ || exit 1
+curl -O https://www.python.org/ftp/python/3.12.3/Python-3.12.3.tgz
+tar -xzf Python-3.12.3.tgz
+cd Python-3.12.3/ || exit 1
 ./configure --enable-optimizations
 make altinstall
 cd ..
-rm -rf Python-3.10.14*
-pip3.10 install --upgrade setuptools pip
+rm -rf Python-3.12.3*
+pip3.12 install --upgrade setuptools pip
 ln -s /usr/local/bin/python3.10 /usr/local/bin/python3
 dnf -y groupremove "Development Tools"
 rm -rf /var/cache/yum/*
