c1fbebd2b2ca1be2ef77114a7ee0c97c64d8136c
Yuming Long <yuming@unstructured.io>
2024-10-17 21:04:38 -0700

    missing places

--- dockerfiles/deps/python.sh
+++ dockerfiles/deps/python.sh
@@ -14,7 +14,7 @@ make altinstall
 cd ..
 rm -rf Python-3.12.3*
 pip3.12 install --upgrade setuptools pip
-ln -s /usr/local/bin/python3.10 /usr/local/bin/python3
+ln -s /usr/local/bin/python3.12 /usr/local/bin/python3
 dnf -y groupremove "Development Tools"
 rm -rf /var/cache/yum/*
 dnf clean all
